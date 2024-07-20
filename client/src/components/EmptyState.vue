3fc2b878415b98d33cc366b9d2ff177c394c8be2
Bilel KIHAL <kb_kihal@esi.dz>
2024-07-20 15:50:54 +0200

    Create EmptyState component

---
+++ client/src/components/EmptyState.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="empty-state-container">
+        <img src="../assets/empty-state.svg" alt="empty state illustration">
+        <div class="empty-state-message">{{ message }}</div>
+    </div>
+    
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+    const props = defineProps({
+    message: {
+        type: String,
+        required: true
+    }
+    });
+</script>
+
+<style scoped>
+    .empty-state-container img{
+        width: 200px;
+    }
+    .empty-state-message{
+        color: gray;
+    }
+</style>
