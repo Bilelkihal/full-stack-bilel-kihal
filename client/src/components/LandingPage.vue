d831c139c1a60bfb5c5da9dffe3a18fe9eea4802
Bilel KIHAL <kb_kihal@esi.dz>
2024-07-20 15:51:04 +0200

    Create landing page component

---
+++ client/src/components/LandingPage.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <img class="pokemon-logo" src="../assets/pokemon.svg" alt="pokemon logo" />
+
+    <div class="card-component">
+        <div class="title">
+            Latest transactions
+        </div>
+        <EmptyState message="No transaction is done yet" />
+    </div>
+    <div class="landing-call-to-actions">
+        <div class="button primary-button" @click="openMarketPage">
+            Enter to the market
+        </div>
+        <div class="button secondary-button" @click="openAllPokemons">
+            Discover all pokemons
+        </div>
+    </div>
+
+</template>
+
+
+<script setup>
+    import EmptyState from './EmptyState.vue';
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+    const openMarketPage = () => {
+        router.push({name: 'Market'});
+    }
+    const openAllPokemons = () => {
+        router.push({name: 'AllPokemons'});
+    }
+</script>
+
+<style scoped>
+    .pokemon-logo{
+        width: 500px;
+    }
+    .landing-call-to-actions{
+        display: flex;
+        justify-content: center;
+    }
+    .landing-call-to-actions div+div{
+        margin-left: 20px;
+    }
+</style>
