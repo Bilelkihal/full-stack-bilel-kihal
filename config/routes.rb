593aa9bbc7e91f5ffa030537995b7b16dd7805ec
Bilel KIHAL <kb_kihal@esi.dz>
2024-07-20 14:21:15 +0200

    add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   resources :transactions
   resources :pokemons
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'me', to: 'users#show_me'
+  get 'me/transactions', to: 'users#transactions'
+
+  get 'pokemon/:id/checkout', to: 'pokemons#checkout'
+  post 'pokemon/:id/buy', to: 'pokemons#buy'
+  post 'pokemon/:id/sell', to: 'pokemons#sell'
+
+
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
