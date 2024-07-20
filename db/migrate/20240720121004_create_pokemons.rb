0bb8f59f4ba02b29ad065b47ac3f48635166a12e
Bilel KIHAL <kb_kihal@esi.dz>
2024-07-20 14:12:36 +0200

    create user, pokemon and transaction scaffolds

---
+++ db/migrate/20240720121004_create_pokemons.rb
@@ -0,0 +1,15 @@
+class CreatePokemons < ActiveRecord::Migration[7.1]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.string :image_path
+      t.integer :weight
+      t.integer :height
+      t.decimal :price
+      t.decimal :last_sell_price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
