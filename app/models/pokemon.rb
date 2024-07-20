222f929e475d6f34b5d9655162ade9b931330c8c
Bilel KIHAL <kb_kihal@esi.dz>
2024-07-20 14:15:45 +0200

    do validators and relations between models

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,8 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
+  has_many :transactions
+
+  validates :name, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :last_sell_price, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :pokemons
+  has_many :transactions
+
+  validates :email, presence: true, uniqueness: true
+  validates :balance, numericality: { greater_than_or_equal_to: 0 }
 end
